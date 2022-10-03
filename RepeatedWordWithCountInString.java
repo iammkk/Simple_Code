0d8c9f3726b8edb4e7fa350c05b243c19c7d05c3
Kiran M <114242860+iammkk@users.noreply.github.com>
2022-10-03 18:48:19 -0500

    Adding new RepeatedWordWithCountInString

---
+++ RepeatedWordWithCountInString.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class RepeatedWordWithCountInString {
+  public static void main(String[] args) {
+    String paragraph = "Bob hit a ball the hit BALL flew far after it was hit";
+    String[] strArray = paragraph.toLowerCase().trim().split("\\s+");
+    System.out.println(Arrays.toString(strArray));
+    Set<String> set = new HashSet<>();
+    for(int i = 0; i<strArray.length; i++){
+      int count =1;
+      for(int j=i+1; j<strArray.length; j++){
+        if(strArray[i].equalsIgnoreCase(strArray[j]) && !set.contains(strArray[i])){
+          count++;
+          set.add(strArray[i]);
+          System.out.print(count+" "+strArray[i]+"\n");
+        }
+      }
+      if(!set.contains(strArray[i]))
+        System.out.print(count+" "+strArray[i]+"\n");
+    }
+  }
+}
